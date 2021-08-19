6397896729f409cac10f165d2fb5c91c4dc1ff5d
rline <crossrun000@gmail.com>
2021-08-19 14:14:51 +0900

    移動制限追加

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -6,6 +6,10 @@ public class PlayerMove : MonoBehaviour
 {
     public float moveSpeed = 18f;
 
+    public float moveLimitX;
+
+    public float moveLimitY;
+
     private Vector2 pos;
 
     void Update()
@@ -19,5 +23,18 @@ public class PlayerMove : MonoBehaviour
         moveY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
         transform.Translate(moveX, moveY, 0);
+
+        MoveClamp();
+    }
+
+    void MoveClamp()
+    {
+        pos = transform.position;
+
+        pos.x = Mathf.Clamp(pos.x, -moveLimitX-4, moveLimitX);
+        pos.y = Mathf.Clamp(pos.y, -moveLimitY, moveLimitY);
+
+        transform.position = pos;
+
     }
 }
